34d7ac074885dfb83e4f3c57f697128dfef059f0
tmvf <tmvf@yandex.ru>
2018-08-12 19:55:36 +0300

    02

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,54 @@
+var messageApi = Vue.resource('/message{/id}');
+
+Vue.component('messages-form', {
+    props: ['messages'],
+    data: function () {
+        return {
+            text: ""
+        }
+    },
+    template: "<div>" +
+    "<input type='text' placeholder='Write something' v-model='text'/>" +
+    "<input type='button' value='Save' @click='save'/>" +
+    "</div>",
+    methods: {
+        save: function () {
+            var message = {text: this.text};
+
+            messageApi.save({}, message)
+                .then(result => result.json().then(data => {
+                this.messages.push(data);
+                this.text=""
+        }) )
+            ;
+        }
+    }
+});
+
+Vue.component('messages-row', {
+    props: ['message'],
+    template: "<div><i>{{message.id}}). </i>{{message.text}}</div>"
+});
+
+Vue.component('messages-list', {
+    props: ['messages'],
+    template: '<div>' +
+    '<messages-form :messages="messages"/>' +
+    '<messages-row v-for="message in messages" :key="message.id" :message="message"/>' +
+    '</div>',
+    created: function () {
+        messageApi.get().then(result => result.json()
+            .then(
+                data => data.forEach(message => this.messages.push(message)
+        )
+        ))
+    }
+});
+
+var app = new Vue({
+    el: '#app',
+    template: '<messages-list :messages="messages"/>',
+    data: {
+        messages: [],
+    }
+});
