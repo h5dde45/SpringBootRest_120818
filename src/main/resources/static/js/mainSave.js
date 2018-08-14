06d17ec486ba0db4613192550937821f6c9f2986
tmvf <tmvf@yandex.ru>
2018-08-15 00:35:54 +0300

    05

--- src/main/resources/static/js/mainSave.js
+++ src/main/resources/static/js/mainSave.js
@@ -1,6 +1,7 @@
 var messageApi = Vue.resource('/message{/id}');
 
 Vue.component('message-form', {
+    props: ['messages'],
     data: function () {
         return {
             text: ""
@@ -9,7 +10,19 @@ Vue.component('message-form', {
     template: "<div>" +
     "<input type='text' placeholder='Write something..' v-model='text'>" +
     "<input type='button' value='Save' @click='save'>" +
-    "</div>"
+    "</div>",
+    methods: {
+        save: function () {
+            let message = {text: this.text};
+
+            messageApi.save({}, message)
+                .then(result =>
+                    result.json().then(data => {
+                        this.messages.push(data);
+                        this.text="";
+                    }))
+        }
+    }
 });
 
 Vue.component('messages-row', {
@@ -20,6 +33,7 @@ Vue.component('messages-row', {
 Vue.component('messages-list', {
     props: ['messages'],
     template: '<div>' +
+    '<message-form :messages="messages"/>' +
     '<messages-row v-for="message in messages" :key="message.id" :message="message"/>' +
     '</div>',
     created: function () {
